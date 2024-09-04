fc1e5fd38d69a8d84239661b84fb98aee4a6bd63
Manan Singhal <23ucs636@lnmiit.ac.in>
2024-09-04 20:39:44 +0530

    vercel.json updated

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,19 @@ const express = require("express");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const cors = require("cors"); // Import the cors package
+const dotenv = require("dotenv");
 
+dotenv.config();
 connectDB();
 
 const app = express();
 
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"], // Allow requests from this origin
+    origin: process.env.FRONTEND_URI, // Allow requests from this origin
     methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
   })
@@ -23,3 +26,5 @@ app.use("/api/teams", require("./routes/teamRoutes"));
 // app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+module.exports(app);
