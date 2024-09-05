a6031800de11c4ef0165ba1718a14fba6f02effe
MananSinghal123 <145357715+MananSinghal123@users.noreply.github.com>
2024-09-05 13:10:27 +0530

    Update Brainfuck.jsx

--- my-react-app/src/pages/Brainfuck.jsx
+++ my-react-app/src/pages/Brainfuck.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./CaesarCipherPage.css";
 import Footer from "./Footer";
-import LnmiitMap from '../../public/LnmiitMap.png';
+import LnmiitMap from '../../public/lnmiitMap.png';
 
 const Brainfuck = () => {
   const [userInput, setUserInput] = useState("");
